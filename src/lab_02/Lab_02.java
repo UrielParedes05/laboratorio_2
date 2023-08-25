e712ab930315251dde2bd3531f0c34d1265aeaa9
UrielParedes05 <142622188+UrielParedes05@users.noreply.github.com>
2023-08-25 13:57:15 -0500

    Add files via upload

---
+++ src/lab_02/Lab_02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_02;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Lab_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.setLocationRelativeTo(null); //Centramos el formulario.
+        calculadora.setVisible(true); //Hacemos visible el formulario
+    }
+    
+}
